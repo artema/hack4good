9de25605639619e0b91466286961ca9b181ffdeb
Artem Abashev <artem@abashev.com>
2014-09-14 13:01:37 +0400

    Code page.

--- server/routes/index.js
+++ server/routes/index.js
@@ -4,7 +4,95 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/', function(req, res){
-	res.render('index.html', {});
+
+	var stats = {
+		my: 17,
+		moscow: 163,
+		world: 122675
+	};
+
+	var feed = [
+		{
+			name: 'Илья Чернышов',
+			img: 'u8.png',
+			address: 'г.Москва, ул. Профсоюзная 117',
+			time: '18:43 13.09.2014'
+		},
+		{
+			name: 'Илья Чернышов',
+			img: 'u8.png',
+			address: 'г.Москва, ул. Профсоюзная 117',
+			time: '18:43 13.09.2014'
+		},
+		{
+			name: 'Илья Чернышов',
+			img: 'u8.png',
+			address: 'г.Москва, ул. Профсоюзная 117',
+			time: '18:43 13.09.2014'
+		},
+		{
+			name: 'Илья Чернышов',
+			img: 'u8.png',
+			address: 'г.Москва, ул. Профсоюзная 117',
+			time: '18:43 13.09.2014'
+		},
+		{
+			name: 'Илья Чернышов',
+			img: 'u8.png',
+			address: 'г.Москва, ул. Профсоюзная 117',
+			time: '18:43 13.09.2014'
+		},
+		{
+			name: 'Илья Чернышов',
+			img: 'u8.png',
+			address: 'г.Москва, ул. Профсоюзная 117',
+			time: '18:43 13.09.2014'
+		},
+		{
+			name: 'Илья Чернышов',
+			img: 'u8.png',
+			address: 'г.Москва, ул. Профсоюзная 117',
+			time: '18:43 13.09.2014'
+		},
+		{
+			name: 'Илья Чернышов',
+			img: 'u8.png',
+			address: 'г.Москва, ул. Профсоюзная 117',
+			time: '18:43 13.09.2014'
+		},
+		{
+			name: 'Илья Чернышов',
+			img: 'u8.png',
+			address: 'г.Москва, ул. Профсоюзная 117',
+			time: '18:43 13.09.2014'
+		},
+		{
+			name: 'Илья Чернышов',
+			img: 'u8.png',
+			address: 'г.Москва, ул. Профсоюзная 117',
+			time: '18:43 13.09.2014'
+		}
+	];
+
+	res.render('index.html', {
+		stats: stats,
+		feed: feed
+	});
+});
+
+router.get('/code', function(req, res){
+	var code = req.query.value;
+
+	if (!code) {
+		res.status(400);
+	  res.end();
+		return;
+	}
+
+	res.render('code.html', {
+		code: code
+	});
+
 });
 
 module.exports = router;
